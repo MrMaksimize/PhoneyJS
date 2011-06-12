3e8404e5222a4f40c48051e0d2902a5952a9fceb
Maksim <max@allplayers.com>
2011-06-12 17:10:00 +0200

    key matches!!

--- phoney.js
+++ phoney.js
@@ -61,15 +61,52 @@
   }
   
   function replaceHref(href, router){
+    //href = href.replace(/(\/|\.|\*)/g, '\\$1');
+    //exp = RegExp(href);
+    if (router[href]){
+      return router[href];
+    }
+    else{
+      
+    for (var key in router){
+        console.log (key + ' ----> ' + router[key]);
+        //regixify url from original link, put it into a variable
+        keyReg = key.replace(/(\/|\.)/g, '\\$1');
+        //replace the star with a regex pattern
+         keyReg = keyReg.replace("*", "(([\\w-_\\.?]+\\/?)+)");
+        exp = RegExp(keyReg); // and make it into a regex
+        //console.log(exp);
+        
+        if (href.match(exp)){ //if regex matches
+          console.log('MATCH')
+          console.log('regex = ' + exp);
+          console.log('href = ' + href);
+          var matchedURL; //href, key
+          matchedURL = router[key];
+          //console.log(matchedURL = matchedURL.split('*')); //splits matched url at *
+          console.log(matchedURL);
+          console.log(href);
+          console.log(href.match(exp));
+          //console.log(matchedURL = )
+          
+          return router[key];
+        }
+        
+        
+        /*console.log(key.match(exp));
+        if(key.match(exp)){
+          console.log(router[key]);
+          return router[key];
+        }*/
+      }
+      }
+  }
+    
     /*here we need to account for a few things:
      what if there's no match?
-     what if it's more than one link?
+     what if it's more than one link depth?
     */
-    console.log('oldhref= ' + href);
-    console.log(router);
-    newHref = router[href];
-    return newHref;
-  }
+  
   
   this.Phoney = function(config) { //pass in objects here
   if(!(this instanceof Phoney)) return new Phoney(config);
@@ -79,6 +116,8 @@
   
   this.startListening = function(){
     var activeRouter = this.activeRouter;
+    //console.log(activeRouter);
+
     window.onclick = function(event){
       console.log(event);
       element = event.srcElement;
@@ -111,7 +150,9 @@ var config = {
       //search : replace
       'http://www.google.com': 'http://www.bing.com',
       'groups.html' : 'groups-html5.html',
-      'index.html' : 'index-html5.html'
+      'index.html' : 'index-html5.html',
+      'somefolder/*': 'somehtml5folder/*', //this one will do matching for all subdirs
+      'somefolder/*/index' : 'somefolder/*/index5'
     }
     
   }
